f74cf1925f91d6110c97fedac32a5a9cf35e6211
jess-izuu <izumijessica@gmail.com>
2020-12-22 19:46:51 -0500

    update WeatherViewController

--- WeatherBuddy/Controller/WeatherViewController.swift
+++ WeatherBuddy/Controller/WeatherViewController.swift
@@ -1,34 +1,90 @@
 //
-//  WeatherViewController.swift
-//  WeatherBuddy
+//  ViewController.swift
+//  Clima
 //
-//  Created by Jessica Izumi on 12/18/20.
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
 //
 
 import UIKit
 
+//UITextFieldDelegate for "Go" Button when search in keyboard
 class WeatherViewController: UIViewController {
 
-    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
     
+    var weatherManager = WeatherManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        weatherManager.delegate = self
+        //TextField should report back to controller, when user types in text field will notify view controller
+        searchTextField.delegate = self
+    }
+
+}
+
+//MARK: - UITextFieldDelegate
+
+
+
+extension WeatherViewController: UITextFieldDelegate {
+   
+    @IBAction func searchPressed(_ sender: UIButton) {
+        //dismisses keyboard after search
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //All of these are triggered by textfield class
+    
+    //what to do when user hits return button on keyboard, true or false to process
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //dismisses keyboard after search
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        return true
     }
-    */
+    
+    //validation on what user types
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //reset search bar after search
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //Use searchTextField.text to get weather for that city
+        if let city = searchTextField.text {
+            weatherManager.fetchWeather(cityName: city)
+        }
+        
+        //Reset to empty string
+        searchTextField.text = ""
+    }
+}
 
+//MARK: - WeatherManagerDelegate
+
+extension WeatherViewController: WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        //print(weather.temp)
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
+    }
 }
