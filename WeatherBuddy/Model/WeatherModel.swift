bc40e96180de86a00f2cc4eea3b11a4dec6ac2b5
jess-izuu <izumijessica@gmail.com>
2020-12-22 19:20:32 -0500

    WeatherModel.swift

---
+++ WeatherBuddy/Model/WeatherModel.swift
@@ -0,0 +1,42 @@
+//
+//  WeatherModel.swift
+//  WeatherBuddy
+//
+//  Created by Jessica Izumi on 12/22/20.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let conditionId: Int
+    let cityName: String
+    let temp: Double
+    
+    var temperatureString: String {
+        return String(format: "%.1f", temp)
+    }
+    
+    // Computed Property
+    var conditionName: String {
+        //Output set as value of property
+        // Switch to change image according to weather ID
+        switch conditionId {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+    }
+}
